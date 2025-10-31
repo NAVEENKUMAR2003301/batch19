73cdd85ce27dafaa3f3c3926f90a538b5c5b61dd
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-10-31 15:29:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,123 @@
+// var - global scope
+// let - block scope
+// const - block scope
+
+// rules
+
+// var - gS
+
+
+var a = 10 ;     // declration // intialiazation
+
+a = 15 ;         // reuse  // reIntialiazation
+
+var a = 20       // redeclration
+
+console.log(a)
+
+// let - block scope
+
+
+let b = 12; // dec // int
+
+b = 15;     // reuse // reint
+
+// let b = 16 // not accept reDec
+
+console.log(b);
+
+// const - block scope
+
+const c = 12 ; // dec //  inti
+
+// c = 15   // not accept reuse, re int
+
+// const c = 18  // not accepted reDecl
+
+console.log(c);
+
+// examples 
+
+
+// var x = 5;
+// var x = 10;
+// console.log(x);  // 10
+
+
+// let y = 20;
+// y = 25;
+// console.log(y); // 25
+
+
+
+
+// const z = 50;
+// z = 60;
+// console.log(z); // error
+
+
+
+
+
+// printing statements
+
+
+// 1. console.log()
+
+// let a1 = 12
+
+// console.log(100);
+
+
+// 2. alert()
+
+// alert("hello team this is our first class")
+
+// 3. confirm()
+  
+// confirm("did you like js")
+
+// 4. prompt()
+
+// prompt("what is your name :-")
+
+// 5. document.writeln()
+
+// document.writeln("hello")
+
+
+// let confirmMsg = confirm("did you like js")
+
+// console.log(confirmMsg);
+
+
+// let userName = prompt("what is your name :-")
+
+// console.log(userName);
+
+
+// console methods
+
+// console.log()
+
+console.log(100);
+
+
+// console.warn()
+
+console.warn(100);
+
+
+// console.error()
+
+console.error(100)
+
+// console.clear()
+
+console.clear()
+
+
+// datatypes
+
+
+
