dd3ec841176a89413103161e838908b60330be1a
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-11-04 12:37:07 +0530

    operators ,  concatination, type conversion

--- script.js
+++ script.js
@@ -273,11 +273,206 @@ let numB = numA++; // 8
 let numC = ++numB; // ++ numB = ++8 = 9
 console.log(numA, numB, numC); // 9,  9 , 9
 
+console.clear();
 
 
 // // 2. Assignment operators
-// // 3. Relational operators
+
+let num1 = 10
+let additionVal = 100
+
+// num1 = num1 + additionVal //== 
+
+ num1 %= additionVal
+
+
+
+
+console.log(num1);
+
+
+// // 3. Relational operators  || comparition operator
+
+    // meaning     operator    example       result 
+
+    // 1. lessThen     <          5<5          false
+    // 2. greater Then >          3>5          false
+    // 3. lessThenEq   <=         5<=5         true
+    // 4. greterThenEq >=         6>=1         true
+    // 5. loosyTypeEq  ==         5=="5"       true
+    // 6. strictlyTypeEq ===      5==="5"      false
+    // 7. loosy Not Eq    !=      5!=5 , 3!=2  true
+    // 8. strictly not Eq  !==    5!=="5"      true 
+
+
+    console.log(7 >= 10);  // false
+    console.log(4 === "4"); // false
+    console.log(6 != 2); // true
+    console.log(8 <= 8); // true 
+    console.log(9 < 5); // false
+    console.log(10 !== "10"); // !false  = true
+    console.log(3 > 3); // false 
+    console.log(5 == "5"); // true
+
+
+    console.clear();
+    
+
 // // 4. logical operators
+
+// AND - &&
+
+// true && true && false = false
+
+// true && true && true  = true  
+
+// OR - ||
+
+// true || false || false = true
+
+// false || false || false = false
+
+// NOT - !
+
+// !(true) = false
+
+
+console.log((5===5) && (5!="5") && (0)); 
+
+// true  && false && false = false
+
+console.log(10 > 5 && 3 < 8 && 2 === "2");
+
+// true && true && false = false
+
+console.log(4 === 4 || 5 > 10 || 6 != 6);
+
+// true || false || false = true 
+
+console.log(!(7 < 9));
+
+// !(true) = false
+
+console.log((2 != 3 && 5 === "5") || 10 > 2);
+
+// (true && false ) || true
+
+// false || true = true
+
+console.log(!(4 > 2 && 6 < 8));
+
+// !(true && true) = !(true) = false
+
+console.log(8 <= 8 || (9 !== "9" && 1));
+
+// true || (true && true) = true || true = true
+
+console.log(0 || (5 && true));
+
+// false || (true && true) = false || true = true
+
+console.log(!(false || (3 == "3" && 2 > 1)));
+
+// (false || (true && true)) = !(false || true) = !(true) = false
+
+
+
+// concatination && template litral
+
+// concatination (+)
+
+let str1 = "hello"
+
+let str2 = "world"
+
+console.log(str1+" "+str2);
+
+// template litral
+
+
+console.log(`${str1} ${str2} hello`);
+
+
+// type conversition
+
+
+// implicit type conversion
+
+let str3 = "11"
+
+let num =  11
+
+console.log(typeof(str3+num));
+
+// string  - (string anything string)
+
+console.log(typeof("1"+"1"));
+console.log(typeof("1"+1));
+console.log(typeof("1"+true));
+console.log(typeof("1"+undefined));
+console.log(typeof("1"+null));
+console.log(typeof("1"+[1,2]));
+console.log(typeof("1"+{k:1}));
+
+// number 
+
+console.log(typeof(1+"1"));
+console.log(typeof(1+1));
+console.log(typeof(1+true));
+console.log(typeof(1+undefined));
+console.log(typeof(1+null));
+console.log(typeof(1+[1,2]));
+console.log(typeof(1+{k:2}));
+
+// Boolean
+
+
+console.log(typeof(true+"1"));
+console.log(typeof(true+1));
+console.log(typeof(true+true));
+console.log(typeof(true+undefined));
+console.log(typeof(true+null));
+console.log(typeof(true+[1,2]));
+console.log(typeof(true+{a:1}));
+
+
+
+// explicit type conversion
+
+//Number - constarctor
+
+console.log(1+Number("1"));
+console.log(Number(""));
+console.log(Number());
+console.log(Number("abc"));
+console.log(Number(1));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(undefined));
+console.log(Number(null));
+console.log(Number([1,2]));
+console.log(Number({a:7}));
+
+
+// Boolean - constractor
+
+
+console.log(Boolean("1"));
+console.log(Boolean(""));
+console.log(Boolean());
+console.log(Boolean("abc"));
+console.log(Boolean(1));
+console.log(Boolean(true));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean([1,2]));
+console.log(Boolean({a:1}));
+
+
+
+
+
+
 
 
 // // function
